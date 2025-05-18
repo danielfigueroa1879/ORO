f4586973de602fd5463ad1140a681a9808315016
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-05-18 19:48:06 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nombre-de-tu-repositorio/',  // Reemplaza con el nombre real de tu repositorio
+  base: '/web_oro/',  // Reemplaza con el nombre real de tu repositorio
 })
